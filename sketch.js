0995e7fd159b73706e49efef309690edbfff9a54
otawfik07 <74074787+otawfik07@users.noreply.github.com>
2021-01-18 23:36:01 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+//level two
+block8 = new Block(330,235,30,40)
+block9 = new Block(360,235,30,40)
+block10 = new Block(390,235,30,40)
+block11 = new Block(420,235,30,40)
+block12 = new Block(450,235,30,40)
+//level three
+block13 = new Block(360,235,30,40)
+block14 = new Block(390,235,30,40)
+block15 = new Block(420,235,30,40)
+//top
+block16 = new Block(390,235,30,40)
